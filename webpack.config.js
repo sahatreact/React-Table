c2e9853cefb85f421642fc055b9056de5c6c4581
unknown <sahatreactjs@gmail.com>
2022-03-12 15:45:59 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+    ],
+  },
+};
